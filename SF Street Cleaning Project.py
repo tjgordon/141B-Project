6e5b5dc4d647b401f09376cb888a708429b98808
Tyler Gordon <tgordon681@gmail.com>
2017-03-03 20:33:32 -0800

    Add more plots

--- SF Street Cleaning Project.py
+++ SF Street Cleaning Project.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[131]:
 
 import pandas as pd
 import seaborn as sns
@@ -9,6 +9,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import feather
+get_ipython().magic('matplotlib inline')
 
 
 # In[20]:
@@ -29,10 +30,15 @@ street_csv = pd.read_csv("Street_and_Sidewalk_Cleaning.csv",
 feather.write_dataframe(street_csv, 'street.feather')
 
 
-# In[26]:
+# In[53]:
 
 # Similar thing built in to pandas. Causes some error.
-#street.to_hdf('street.h5','table',append=False)
+street.to_hdf('street.h5','table',append=False)
+
+
+# In[56]:
+
+#street2 = pd.read_hdf('street.h5')
 
 
 # In[24]:
@@ -40,7 +46,7 @@ feather.write_dataframe(street_csv, 'street.feather')
 street = feather.read_dataframe('street.feather')
 
 
-# In[27]:
+# In[57]:
 
 # To use the csv version
 # street = street_csv
@@ -65,7 +71,7 @@ print "We are working with", numRows, "rows."
 print "Our dates range from", street.loc[numRows - 1, "Opened"],"to", street.loc[0, "Opened"], "."
 
 
-# In[29]:
+# In[63]:
 
 #plt.figure(figsize=(2,100)) # Doesn't do much
 theOrder = ["Voice In", "Open311", "Web Self Service", "Integrated Agency", "Twitter", "e-mail In", "Other Department"]
@@ -99,18 +105,125 @@ street.Neighborhood.unique()
 street.Neighborhood.value_counts
 
 
-# In[34]:
+# In[59]:
 
 # From: http://stackoverflow.com/questions/22391433/count-the-frequency-that-a-value-occurs-in-a-dataframe-column
 counts = street.groupby('Neighborhood').count()
-counts.head()
 
 
 # We can get the total number of cases from CaseID
 # unresolved cases by neighborhood
 
-# In[52]:
+# In[90]:
 
-counts.sort_values(by = "CaseID",
-            ascending = False)
+counts = counts.sort_values(by = "CaseID",
+                            ascending = False)
+counts = counts.reset_index()
 
+
+# In[91]:
+
+counts.head()
+
+
+# In[101]:
+
+sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
+ax = sns.factorplot(x = "CaseID", 
+                    y = "Neighborhood",
+                    data = counts.head(15), 
+                    kind = "bar", 
+                    orient = "h", 
+                    aspect = 3
+                   )#, size = 10)
+plt.title("Requests by Neighborhood (Top 15 Neighborhoods)") 
+plt.show()
+
+
+# In[109]:
+
+counts['UnclosedProp'] = (counts.Opened - counts.Closed) / counts.Opened
+
+
+# In[110]:
+
+counts.head()
+
+
+# In[112]:
+
+sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
+ax = sns.factorplot(x = "UnclosedProp", 
+                    y = "Neighborhood",
+                    data = counts.sort_values(by = "UnclosedProp",
+                                              ascending = False).head(15), 
+                    kind = "bar", 
+                    orient = "h", 
+                    aspect = 3
+                   )#, size = 10)
+plt.title("Proportion of Unclosed Requests by Neighborhood (Top 15 Neighborhoods)") 
+plt.show()
+
+
+# Use supervisor district where there are too many neighborhoods. 
+
+# In[125]:
+
+request_counts = street.groupby(by = "Request Type").count().reset_index().ix[:,["Request Type","CaseID"]].sort_values(by = "CaseID", ascending = False)
+request_counts.head()
+
+
+# In[132]:
+
+sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
+ax = sns.factorplot(y = "Request Type", 
+                    x = "CaseID",
+                    data = request_counts, 
+                    kind = "bar", 
+                    orient = "h", 
+                    aspect = 3
+                   )#, size = 10)
+plt.title("Requests Type by Neighborhood (Top 15 Neighborhoods)") 
+plt.show()
+
+
+# Differences by time of year:
+# - Mattresses in summer  
+# - Holiday shopping  
+# 
+
+# Note: only use 2009 through 2016 to only count full years.  
+# Ask TA if we should do this for all analysis or just this part.
+
+# In[139]:
+
+street['month'] = [timestamp.month for timestamp in street.Opened]
+
+
+# In[140]:
+
+street.head()
+
+
+# In[148]:
+
+count_by_month = street.groupby(by='month').count().CaseID.reset_index()
+count_by_month
+
+
+# In[150]:
+
+sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
+ax = sns.factorplot(x = "CaseID", 
+                    y = "month",
+                    data = count_by_month, 
+                    kind = "bar", 
+                    orient = "h", 
+                    aspect = 3
+                   )#, size = 10)
+plt.title("Requests Type by Neighborhood (Top 15 Neighborhoods)") 
+plt.show()
+
+
+# Faster at closing requests by time?
+# Time to close requests by neighborhood?
