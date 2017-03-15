4c44efaed497b5c5b3261167073f311c99c362e2
Tyler Gordon <tgordon681@gmail.com>
2017-03-14 19:57:49 -0700

    Start maps

--- SF Street Cleaning Project.py
+++ SF Street Cleaning Project.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 import pandas as pd
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
-import feather
+#import feather
 import requests
 import requests_cache
 import lxml
@@ -34,23 +34,29 @@ street_csv = pd.read_csv("Street_and_Sidewalk_Cleaning.csv",
 
 # In[ ]:
 
+# Write feather
 feather.write_dataframe(street_csv, 'street.feather')
 
 
 # In[ ]:
 
-# Similar thing built in to pandas. Causes some error.
+# Write h5
 street.to_hdf('street.h5','table',append=False)
 
 
 # In[2]:
 
+# Katherine run only this
 #street2 = pd.read_hdf('street.h5')
+street2 = pd.HDFStore('street.h5')
+street = street2.select('/table')
+street.head()
 
 
 # In[3]:
 
-street = feather.read_dataframe('street.feather')
+# Read feather
+#street = feather.read_dataframe('street.feather')
 
 
 # In[4]:
@@ -61,6 +67,7 @@ street = feather.read_dataframe('street.feather')
 
 # In[6]:
 
+# Check if the binary version is equivalent
 #all(street == street_csv)
 
 
@@ -69,8 +76,9 @@ street = feather.read_dataframe('street.feather')
 street.head()
 
 
-# In[37]:
+# In[3]:
 
+# TODO: Only use this subset for some analysis, since some major events are outside this range. 
 street = street.loc[street['Opened'].dt.year != 2008]
 street = street.loc[street['Opened'].dt.year != 2017]
 street = street.sort_values("Opened")
@@ -81,14 +89,14 @@ street.head()
 
 # Some basic statistics on the dataset we are starting with:
 
-# In[50]:
+# In[4]:
 
 numRows = street.shape[0]
 print "We are working with", numRows, "rows."
 print "Our dates range from", street.loc[numRows - 1, "Opened"],"to", street.loc[0, "Opened"], "."
 
 
-# In[91]:
+# In[5]:
 
 #plt.figure(figsize=(2,100)) # Doesn't do much
 theOrder = ["Voice In", "Open311", "Web Self Service", "Integrated Agency", "Twitter", "e-mail In", "Other Department"]
@@ -101,17 +109,17 @@ plt.show()
 
 # According to [the project's website](http://www.open311.org/learn/), Open311 allows people to report issues in public spaces to city officials through a [website](https://sf311.org/index.aspx?page=797) or [mobile app](https://www.sf311.org/mobile).  
 
-# In[52]:
+# In[6]:
 
 street.Neighborhood.unique()
 
 
-# In[53]:
+# In[7]:
 
 street.Neighborhood.value_counts
 
 
-# In[54]:
+# In[8]:
 
 # From: http://stackoverflow.com/questions/22391433/count-the-frequency-that-a-value-occurs-in-a-dataframe-column
 counts = street.groupby('Neighborhood').count()
@@ -120,19 +128,19 @@ counts = street.groupby('Neighborhood').count()
 # We can get the total number of cases from CaseID
 # unresolved cases by neighborhood
 
-# In[55]:
+# In[9]:
 
 counts = counts.sort_values(by = "CaseID",
                             ascending = False)
 counts = counts.reset_index()
 
 
-# In[56]:
+# In[10]:
 
 counts.head()
 
 
-# In[57]:
+# In[11]:
 
 sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
 ax = sns.factorplot(x = "CaseID", 
@@ -147,7 +155,7 @@ plt.title("Requests by Neighborhood (Top 15 Neighborhoods)")
 plt.show()
 
 
-# In[58]:
+# In[12]:
 
 sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
 ax = sns.factorplot(x = "CaseID", 
@@ -162,17 +170,17 @@ plt.title("Requests by Neighborhood (Bottom 15 Neighborhoods)")
 plt.show()
 
 
-# In[59]:
+# In[13]:
 
 counts['UnclosedProp'] = (counts.Opened - counts.Closed) / counts.Opened
 
 
-# In[60]:
+# In[14]:
 
 counts.head()
 
 
-# In[88]:
+# In[15]:
 
 sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
 ax = sns.factorplot(x = "UnclosedProp", 
@@ -189,13 +197,13 @@ plt.show()
 
 # Use supervisor district where there are too many neighborhoods. 
 
-# In[62]:
+# In[16]:
 
 request_counts = street.groupby(by = "Request Type").count().reset_index().ix[:,["Request Type","CaseID"]].sort_values(by = "CaseID", ascending = False)
 request_counts.head()
 
 
-# In[90]:
+# In[17]:
 
 sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
 ax = sns.factorplot(y = "Request Type", 
@@ -217,23 +225,23 @@ plt.show()
 # Note: only use 2009 through 2016 to only count full years.  
 # Ask TA if we should do this for all analysis or just this part.
 
-# In[64]:
+# In[18]:
 
 street['month'] = [timestamp.month for timestamp in street.Opened]
 
 
-# In[65]:
+# In[19]:
 
 street.head()
 
 
-# In[66]:
+# In[20]:
 
 count_by_month = street.groupby(by='month').count().CaseID.reset_index()
 count_by_month
 
 
-# In[67]:
+# In[21]:
 
 sns.set_context("notebook", rc={"font.size" : 40}) # font_scale=1.5
 ax = sns.pointplot(y = "CaseID", 
@@ -248,7 +256,7 @@ plt.title("Requests by Month")
 plt.show()
 
 
-# In[68]:
+# In[22]:
 
 count_by_month.plot(y = "CaseID", 
                     x = "month")
@@ -257,7 +265,7 @@ count_by_month.plot(y = "CaseID",
 # Faster at closing requests by time?
 # Time to close requests by neighborhood?
 
-# In[92]:
+# In[23]:
 
 street['year'] = [timestamp.year for timestamp in street.Opened]
 count_by_year = street.groupby(by='year').count().CaseID.reset_index()
@@ -274,41 +282,41 @@ plt.title("Requests by Year")
 plt.show()
 
 
-# In[69]:
+# In[24]:
 
 [(colname, len(street[colname].unique())) for colname in list(street)]
 
 
-# In[70]:
+# In[25]:
 
 by_month_req_type = street.groupby(by=['month','Request Type']).count().CaseID.reset_index()
 by_month_req_type = by_month_req_type.sort_values(by = ['month', "CaseID"], ascending=[True,False])
 by_month_req_type.head()
 
 
-# In[71]:
+# In[26]:
 
 #by_month_req_type = street.groupby(by=['month','Request Type']).plot()
 
 
-# In[72]:
+# In[27]:
 
 #street.groupby(by=['month','Request Type']).plot(y = 'CaseID', x = 'month')
 
 
-# In[73]:
+# In[28]:
 
 by_month_req_type.plot(y = 'CaseID', x = 'month')
 
 
 # # Scraping
 
-# In[74]:
+# In[29]:
 
 requests_cache.install_cache('sf_cache')
 
 
-# In[75]:
+# In[30]:
 
 url = "http://www.city-data.com/nbmaps/neigh-San-Francisco-California.html"
 response = requests.get(url)
@@ -319,22 +327,22 @@ neighborhoods_bs = BeautifulSoup(response.text, 'lxml')
 neighborhood_names = neighborhoods_bs.find_all(name = "span", attrs={'class':'street-name'})
 
 
-# In[76]:
+# In[31]:
 
 neighborhood_names = [name.text for name in neighborhood_names]
 
 
-# In[77]:
+# In[32]:
 
 neighborhood_names
 
 
-# In[78]:
+# In[33]:
 
 neighborhood_divs = neighborhoods_bs.body.find_all(name = "div", attrs={'class':'neighborhood'})
 
 
-# In[79]:
+# In[34]:
 
 neighborhood_divs[0].text
 
@@ -342,31 +350,31 @@ neighborhood_divs[0].text
 # regular expressions
 # [capital letter][lowercase][:][ ][numbers or , or $]
 
-# In[80]:
+# In[35]:
 
 neighborhood_divs[0].find_all(name = "b")
 
 
-# In[81]:
+# In[36]:
 
 neighborhood_divs[0].contents
 
 
-# In[82]:
+# In[37]:
 
 type(neighborhood_divs[0].contents[1])
 
 
 # Loop through contents  
 # if a navigable string can be converted to int,  
-# then grab it and the first <b> that precedes it  
+# then grab it and the first that precedes it  
 
-# In[83]:
+# In[38]:
 
 neighborhood_divs[0].strings
 
 
-# In[84]:
+# In[39]:
 
 # Add to a list of strings
 strings = []
@@ -377,7 +385,7 @@ for descendant in neighborhood_divs[0].strings:
 strings
 
 
-# In[85]:
+# In[40]:
 
 #contents = neighborhood_divs[0].contents
 
@@ -418,12 +426,85 @@ for i in range(1, len(strings)):
 value_dict_list
 
 
-# In[86]:
+# In[41]:
 
 strings
 
 
-# In[87]:
+# In[42]:
 
 int("9,999")
+
+
+# --------
+
+# ## Events and Festivals
+
+# In[ ]:
+
+
+
+
+# --------
+
+# ## Maps
+
+# In[44]:
+
+import folium
+
+
+# In[82]:
+
+# Coordinates from https://en.wikipedia.org/wiki/San_Francisco and 
+# http://andrew.hedges.name/experiments/convert_lat_long/
+m = folium.Map(location=[37.783, -122.416], zoom_start=12)
+m
+
+
+# In[83]:
+
+# Points
+street.ix[1,'Point']
+
+
+# In[84]:
+
+def to_coordinates(point):
+    """
+    Converts a string in the format '(37.7695911772607, -122.415577110949)' to coordinates.
+    """
+    
+    # Tests
+    assert(isinstance(point, str) and point.startswith('(') and point.endswith(')'))
+    
+    (lat, lon) = point.split(',')
+    
+    # Remove '('
+    lat = lat[1:]
+    
+    # Remove ')' and space
+    lon = lon[:-1].strip()
+    
+    
+    
+    return (float(lat), float(lon))
+    
+to_coordinates(street.ix[1,'Point'])
+
+
+# In[92]:
+
+# TODO: Make this a function
+folium.Marker(to_coordinates(street.ix[1,'Point']), popup = street.ix[1,'Request Type']).add_to(m)
+m
+
+
+# In[94]:
+
+# Neighborhood geojson from 
+# https://data.sfgov.org/Geographic-Locations-and-Boundaries/Analysis-Neighborhoods/p5b7-5n3h
+
+folium.GeoJson(open('Analysis Neighborhoods.geojson'), name='geojson').add_to(m)
+m
 
